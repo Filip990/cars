d782bfbcb0aec8991a8e6539fae3aa8c17e8ffa0
Filip990 <filipfilipovic.stray@gmail.com>
2020-06-23 14:34:20 +0200

    updated scale layout

--- src/components/Scale/Scale.styled.js
+++ src/components/Scale/Scale.styled.js
@@ -3,14 +3,14 @@ import styled from "styled-components";
 export const ScaleContainer = styled.div`
 	border: 1px solid black;
 	border-collapse: collapse;
-	display: table;
+	display: grid;
+	grid-template-rows: repeat(3, 1fr);
 	width: 100%;
 	height: 12rem;
 `;
 
 export const Track = styled.div`
 	border: 1px solid black;
-	display: table-row;
 	text-align: left;
 `;
 
